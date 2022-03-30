e7525933e2e25086e0632bf312ca108ed3b51a4f
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-03-30 16:50:25 -0300

    add login style

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -13,6 +13,7 @@ import useTranslation from 'hooks/useTranslation';
 import routesPaths from 'routes/routesPaths';
 import Input from 'components/form/Input';
 import Button from 'components/common/Button';
+import smiles from '../../assets/smiles.svg';
 
 const Login = () => {
   const t = useTranslation();
@@ -51,36 +52,46 @@ const Login = () => {
   }
 
   return (
-    <div className="form">
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <h1>{t('login.title')}</h1>
-        <label htmlFor="email">{t('login.labels.email')}</label>
-        <Input
-          register={register}
-          type="email"
-          name="email"
-          error={errors.email}
-          handleFocus={handleFocus}
-        />
+    <div className="login">
+      <div className="login__header">
+        <img className="login__smiles" src={smiles} alt="smiling faces" />
+        <h2 className="login__title">Target MVD</h2>
+        <h4 className="login__subtitle">Find people near you & Connect</h4>
+        <p className="login__paragraph">
+          Create a target wherever on the map, specify your interest: Travel, Dating, Music, etc and
+          start conecting with others who share your interest.
+        </p>
+      </div>
+      <div className="login__form-container">
+        <form className="login__form" onSubmit={handleSubmit(onSubmit)} noValidate>
+          <label htmlFor="email">{t('login.labels.email')}</label>
+          <Input
+            register={register}
+            type="email"
+            name="email"
+            error={errors.email}
+            handleFocus={handleFocus}
+          />
 
-        <label htmlFor="password">{t('login.labels.password')}</label>
-        <Input
-          register={register}
-          type="password"
-          name="password"
-          error={errors.password}
-          handleFocus={handleFocus}
-        />
+          <label htmlFor="password">{t('login.labels.password')}</label>
+          <Input
+            register={register}
+            type="password"
+            name="password"
+            error={errors.password}
+            handleFocus={handleFocus}
+          />
 
-        {error && error.data && <p className="error-message">{error.data.errors}</p>}
+          {error && error.data && <p className="error-message">{error.data.errors}</p>}
 
-        <div className="button-container">
           <Button type="submit" disabled={isLoading}>
             {t('login.title')}
           </Button>
-          <Link to={routesPaths.signup}>{t('login.dontHaveAccountMsg')}</Link>
-        </div>
-      </form>
+          <Link className="login__signup-link" to={routesPaths.signup}>
+            {t('login.dontHaveAccountMsg')}
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
