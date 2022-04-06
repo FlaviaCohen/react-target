58b47779b93645c30a7f7f404f8e69c0e08af946
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-04-06 12:57:26 -0300

    add welcome message and map

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import Button from 'components/common/Button/Button';
 import useTranslation from 'hooks/useTranslation';
 import { useLogoutMutation } from 'services/auth/auth';
 
-import logo from 'assets/logo.svg';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Home = () => {
   const t = useTranslation();
@@ -12,12 +12,31 @@ const Home = () => {
 
   return (
     <div className="home">
-      <img src={logo} className="home__logo" alt={t('home.logoAltMsg')} />
-      <h1>{t('home.welcomeMsg')}</h1>
-      <div className="home__logout">
-        <Button handleClick={handleLogout} disabled={isLoading}>
-          {t('home.logoutBtn')}
-        </Button>
+      <div className="home__welcome">
+        <h1>{t('home.welcomeMsg')}</h1>
+        <h2>{t('home.title')}</h2>
+        <ul>
+          <li>{t('home.descriptionItem1')}</li>
+          <li>{t('home.descriptionItem2')}</li>
+        </ul>
+        <div className="home__logout">
+          <Button handleClick={handleLogout} disabled={isLoading}>
+            {t('home.logoutBtn')}
+          </Button>
+        </div>
+      </div>
+      <div>
+        <MapContainer center={[51.505, -0.09]} zoom={13}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
