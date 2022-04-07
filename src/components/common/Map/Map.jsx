de40e086e6ac52529a128bbb468e733b44f4ab35
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-04-07 18:55:16 -0300

    create instance of map with current location

--- src/components/common/Map/Map.jsx
+++ src/components/common/Map/Map.jsx
@@ -1,38 +1,12 @@
-import { useState, useEffect } from 'react';
-import { MapContainer, Marker, TileLayer, MapConsumer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
-import { Icon } from 'leaflet';
-import marker from 'assets/marker.svg';
-
-const markerIcon = new Icon({
-  iconUrl: marker,
-  iconRetinaUrl: marker,
-  popupAnchor: [-0, -0],
-  iconSize: [53, 69],
-});
+import LocationMarker from './LocationMarker';
 
 const Map = () => {
-  const [position, setPosition] = useState({ lat: 0, lng: 0 });
-
-  const setCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        setPosition({ lat: position.coords.latitude, lng: position.coords.longitude });
-      },
-      null,
-      { enableHighAccuracy: true }
-    );
-  };
-
-  useEffect(() => {
-    setCurrentLocation();
-  }, []);
-
   return (
-    <MapContainer className="map__container" center={position} zoom={3}>
+    <MapContainer className="map__container" center={[0, 0]} zoom={15}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} icon={markerIcon}></Marker>
+      <LocationMarker />
     </MapContainer>
   );
 };
