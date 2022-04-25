7ad6cf55ed5e91d62fd0d3559934490d1e9c51f3
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-04-25 18:13:46 -0300

    add contact structure

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,12 @@
+import Input from 'components/form/Input/Input';
+import Button from 'components/common/Button/Button';
+import useTranslation from 'hooks/useTranslation';
 import close from 'assets/close.svg';
+import smiles from 'assets/smiles.svg';
 
 const Contact = ({ isContactOpen, handleContact }) => {
+  const t = useTranslation();
+
   return (
     <div
       className={`contact ${isContactOpen ? '' : 'hidden'}`}
@@ -10,15 +16,29 @@ const Contact = ({ isContactOpen, handleContact }) => {
       tabIndex={0}
     >
       <div
-        className="contact__close"
+        className="contact__content"
         onClick={handleContact}
         onKeyPress={handleContact}
         role="button"
         tabIndex={0}
       >
-        <img src={close} alt="close" />
+        <div className="contact__close">
+          <img src={close} alt="close" />
+        </div>
+        <img src={smiles} alt="smiles" className="contact__smiles" />
+        <h1 className="contact__title">{t('contact.title')}</h1>
+        <form action="" className="form contact__form">
+          <label htmlFor="" className="form__label">
+            {t('contact.emailLabel')}
+          </label>
+          {/*    <Input name="email" type="text" /> */}
+          <label htmlFor="" className="form__label">
+            {t('contact.msgLabel')}
+          </label>
+          {/*           <Input name="message" type="text" /> */}
+          <Button>{t('contact.sendBtn')}</Button>
+        </form>
       </div>
-      Contact Modal
     </div>
   );
 };
