0ec8c1a5782f12c33a1fb0c5bf574c5f4baa008e
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-04-28 12:02:55 -0300

    create new target marker

--- src/components/common/Map/LocationMarker.jsx
+++ src/components/common/Map/LocationMarker.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Marker, Popup, useMap } from 'react-leaflet';
 import { Icon } from 'leaflet';
 import marker from 'assets/marker.svg';
+import newTarget from 'assets/newTarget.svg';
 
 const markerIcon = new Icon({
   iconUrl: marker,
@@ -10,24 +11,50 @@ const markerIcon = new Icon({
   iconSize: [53, 69],
 });
 
+const targetIcon = new Icon({
+  iconUrl: newTarget,
+  iconRetinaUrl: newTarget,
+  popupAnchor: [-0, -0],
+  iconSize: [46, 46],
+});
+
 const LocationMarker = () => {
   const [position, setPosition] = useState(null);
-
+  const [newTarget, setNewTarget] = useState(null);
   const map = useMap();
 
-  useEffect(() => {
+  const findLocation = () => {
     map.locate().on('locationfound', function (e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
     });
+  };
+
+  const handleClick = e => {
+    map.on('click', ev => {
+      let latlng = map.mouseEventToLatLng(ev.originalEvent);
+      setNewTarget({ lat: latlng.lat, lng: latlng.lng });
+    });
+  };
+
+  useEffect(() => {
+    findLocation();
+    handleClick();
   }, [map]);
 
   return (
-    position && (
-      <Marker position={position} icon={markerIcon}>
-        <Popup>You are here.</Popup>
-      </Marker>
-    )
+    <>
+      {position && (
+        <Marker position={position} icon={markerIcon}>
+          <Popup>You are here.</Popup>
+        </Marker>
+      )}
+      {newTarget && (
+        <Marker position={newTarget} icon={targetIcon}>
+          <Popup>You are here.</Popup>
+        </Marker>
+      )}
+    </>
   );
 };
 
