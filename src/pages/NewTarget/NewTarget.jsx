7cae17582c7a4d815fac223e5229ac9d75caa3fe
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-04-28 13:57:57 -0300

    add locales text

--- src/pages/NewTarget/NewTarget.jsx
+++ src/pages/NewTarget/NewTarget.jsx
@@ -1,25 +1,28 @@
+import useTranslation from 'hooks/useTranslation';
 import Input from 'components/form/Input/Input';
 import Button from 'components/common/Button/Button';
 import target from 'assets/target.svg';
 import smiles from 'assets/smiles.svg';
 
 const NewTarget = () => {
+  const t = useTranslation();
+
   return (
     <div className="new">
       <img src={target} alt="target" className="new__target-icon" />
-      <p className="new__title">Create new target</p>
+      <p className="new__title">{t('newTarget.title')}</p>
 
       <form className="new__form">
         <label htmlFor="area" className="form__label new__label">
-          Specify area length
+          {t('newTarget.labels.area')}
         </label>
         {/* <Input name="area"></Input> */}
         <label htmlFor="title" className="form__label new__label">
-          Target title
+          {t('newTarget.labels.title')}
         </label>
         {/*  <Input name="title"></Input> */}
         <label htmlFor="topic" className="form__label new__label">
-          Select a topic
+          {t('newTarget.labels.topic')}
         </label>
         <select name="topic" className="new__select">
           <option className="new__option"></option>
