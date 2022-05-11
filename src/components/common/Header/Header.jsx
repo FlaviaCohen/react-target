bd72bf3e62485858f2344fb266dd1c8bafe9a3e7
FlaviaCohen <flaviacohen@FlaviaCohens-MacBook-Pro.local>
2022-05-11 12:34:10 -0300

    add header section style and goback

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import routesPaths from 'routes/routesPaths';
 import Contact from 'components/Contact/Contact';
 import useTranslation from 'hooks/useTranslation';
 import hamburger from 'assets/hamburger.svg';
 import close from 'assets/close.svg';
+import back from 'assets/back.svg';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isContactOpen, setIsContactOpen] = useState(false);
 
   const t = useTranslation();
+  const location = useLocation();
+  const history = useHistory();
 
   const handleClick = () => {
     setIsOpen(!isOpen);
@@ -19,17 +22,35 @@ const Header = () => {
   const handleContact = () => {
     setIsContactOpen(!isContactOpen);
   };
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
   return (
-    <header className="header">
-      <div
-        className="header__icon-container"
-        role="button"
-        onClick={handleClick}
-        onKeyPress={handleClick}
-        tabIndex={0}
-      >
-        <img src={isOpen ? close : hamburger} alt="menu icon" className="header__icon" />
-      </div>
+    <header className={`header ${location.pathname == '/new-target' ? 'bg-lb' : 'bg-w'}`}>
+      {location.pathname == '/new-target' ? (
+        <div className="header__actions">
+          <img
+            src={back}
+            alt="go back"
+            className="header__icon"
+            onClick={handleGoBack}
+            role="presentation"
+          />
+          <h1 className="header__title">{t('header.newTarget')}</h1>
+        </div>
+      ) : (
+        <div
+          className="header__icon-container"
+          role="button"
+          onClick={handleClick}
+          onKeyPress={handleClick}
+          tabIndex={0}
+        >
+          <img src={isOpen ? close : hamburger} alt="menu icon" className="header__icon" />
+        </div>
+      )}
       {isOpen && (
         <div className="header__menu">
           <div className="header__nav">
